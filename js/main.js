b9d94baade60b79d959242ae22d187dd8079b419
AlexeyTelenik <51457024+CortezCardinal@users.noreply.github.com>
2021-02-22 17:11:59 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+	$(".logo__title").click(function( event ) {
+  event.preventDefault(); 
+});
+	const associate__carousel = $('#associate-body__carousel');
+    const left = $('#sliderLeft');
+    const right = $('#sliderRight');
+    const owl = $('.owl-carousel');
+
+left.click(function(){
+    associate__carousel.trigger('prev.owl.carousel');
+})
+
+right.click(function(){
+associate__carousel.trigger('next.owl.carousel');
+})
+  $(".owl-carousel").owlCarousel({
+  	loop:true,
+    margin:16,
+    responsive:{
+        0:{
+            items:1
+        },
+        385:{
+            items:1
+        },
+        900:{
+            items:4
+        }
+    }
+  	});
+/*$( ".sub-menu__items a" ).click(function( e ){e.preventDefault();});*/
+$( ".footer-body__description-item a" ).click(function( e ){e.preventDefault();});
+$( ".footer-body__description-social a" ).click(function( e ){e.preventDefault();});
+$(".sender-body__info-circle").click(function( event ) {
+ $(".sender-body__info-circle").toggleClass("active-circle-circle");
+$(".sender-body__info-title").toggleClass("active-circle");
+ $(".send-body-form2").toggleClass("send-body-form2-active");
+});
+   $(".burger-menu").click(function( event ) {
+  $(".header__menu-bg").addClass("header__menu-bg-active");
+ $(".menu-items").addClass("menu-items-active");
+ $("body").addClass("body-active-menu");
+ 
+ });
+   $(".menu-burger-cross").click(function( event ) {
+  $(".header__menu-bg").removeClass("header__menu-bg-active");
+ $(".menu-items").removeClass("menu-items-active");
+ $("body").removeClass("body-active-menu");
+ });
+});
+ 
